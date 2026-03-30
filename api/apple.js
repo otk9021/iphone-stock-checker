639b5368d137e802a9d3ddb3091d0dcd5862908e
otk9021 <70105911+otk9021@users.noreply.github.com>
2026-03-31 00:29:37 +0900

    Update apple.js

--- api/apple.js
+++ api/apple.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "OK" });
+export default async function handler(req, res) {
+  await fetch(process.env.DISCORD_WEBHOOK, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: "テスト通知OK🔥",
+    }),
+  });
+
+  res.status(200).json({ message: "sent" });
 }
